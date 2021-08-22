45cc61748292909053027a4c4e6a3908daf1dd9b
Abdul Khoir <abdul@investree.id>
2021-08-23 01:21:29 +0700

    AHUPP-000: winstonAdaptor logging from input and default all

--- src/index.ts
+++ src/index.ts
@@ -75,7 +75,7 @@ export const connection = async (config: ConnectionOptions) => {
     const connectionOptions: ConnectionOptions = {
         ...config,
         entities: [__dirname + '/entity/**/*'],
-        logger: config.logger || new WinstonAdaptor(new Logger().elasticLogger, 'all'),
+        logger: config.logger || new WinstonAdaptor(new Logger().elasticLogger, config.logging || 'all'),
     };
     return await createConnection(connectionOptions);
 };
